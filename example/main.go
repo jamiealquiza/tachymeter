ba97d72e746342f8fea3c663c043841b72780faa
Jamie Alquiza <jamiealquiza@gmail.com>
2016-10-10 10:14:52 -0600

    adds example file

---
+++ example/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/jamiealquiza/tachymeter"
+)
+
+func main() {
+	c := tachymeter.New(&tachymeter.Config{Size: 100})
+
+	for i := 0; i < 100; i++ {
+		start := time.Now()
+		time.Sleep(time.Duration(rand.Intn(30)) * time.Millisecond)
+		c.AddTime(time.Since(start))
+		c.AddCount(1)
+	}
+
+	results := c.Json()
+	fmt.Println(results)
+
+	c.Dump()
+}
