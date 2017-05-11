1ecd3969edbbbe45d668a660b08c3ee843e85ef3
Jamie Alquiza <jamiealquiza@gmail.com>
2017-05-11 08:54:48 -0600

    updated example

--- example/tachymeter-graphing/main.go
+++ example/tachymeter-graphing/main.go
@@ -12,20 +12,26 @@ func main() {
 	c := tachymeter.New(&tachymeter.Config{Size: 50})
 	tl := tachymeter.Timeline{}
 
+	// Run 3 iterations of a loop that we're
+	// interesting in summarizing with tachymeter.
 	for iter := 0; iter <3; iter++ {
 		fmt.Printf("Running iteration %d\n", iter)
+		// Capture timing data from the loop.
 		for i := 0; i < 100; i++ {
 			start := time.Now()
 			time.Sleep(time.Duration(rand.Intn(30)) * time.Millisecond)
 			c.AddTime(time.Since(start))
 		}
 
+		// Add each loop tachymeter
+		// to the event timeline.
 		tl.AddEvent(c.Calc())
 		c.Reset()
 	}
 
 
-	// Create an HTML graph of the event histogram.
+	// Write out an HTML page with the
+	// histogram for all iterations.
 	err := tl.WriteHtml(".")
 	if err != nil {
 		fmt.Println(err)
