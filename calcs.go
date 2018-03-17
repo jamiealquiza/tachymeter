5a42db0f374dd7655172c276b31cb511108b998a
Jamie Alquiza <jamiealquiza@gmail.com>
2018-03-17 12:43:27 -0600

    added population standard deviation

--- calcs.go
+++ calcs.go
@@ -48,43 +48,44 @@ func (m *Tachymeter) Calc() *Metrics {
 	metrics.Time.Min = times.min()
 	metrics.Time.Max = times.max()
 	metrics.Time.Range = times.srange()
+	metrics.Time.StdDev = times.stdDev()
 
-	metrics.Histogram, metrics.HistogramBucketSize = times.hgram(m.HBuckets)
+	metrics.Histogram, metrics.HistogramBinSize = times.hgram(m.HBins)
 
 	return metrics
 }
 
 // hgram returns a histogram of event durations in
-// b buckets, along with the bucket size.
+// b bins, along with the bin size.
 func (ts timeSlice) hgram(b int) (*Histogram, time.Duration) {
 	res := time.Duration(1000)
-	// Interval is the time range / n buckets.
+	// Interval is the time range / n bins.
 	interval := time.Duration(int64(ts.srange()) / int64(b))
 	high := ts.min() + interval
 	low := ts.min()
 	max := ts.max()
 	hgram := &Histogram{}
-	pos := 1 // Bucket position.
+	pos := 1 // Bin position.
 
 	bstring := fmt.Sprintf("%s - %s", low/res*res, high/res*res)
-	bucket := map[string]uint64{}
+	bin := map[string]uint64{}
 
 	for _, v := range ts {
-		// If v fits in the current bucket,
-		// increment the bucket count.
+		// If v fits in the current bin,
+		// increment the bin count.
 		if v <= high {
-			bucket[bstring]++
+			bin[bstring]++
 		} else {
-			// If not, prepare the next bucket.
-			*hgram = append(*hgram, bucket)
-			bucket = map[string]uint64{}
+			// If not, prepare the next bin.
+			*hgram = append(*hgram, bin)
+			bin = map[string]uint64{}
 
 			// Update the high/low range values.
 			low = high + time.Nanosecond
 
 			high += interval
 			// if we're going into the
-			// last bucket, set high to max.
+			// last bin, set high to max.
 			if pos == b-1 {
 				high = max
 			}
@@ -92,20 +93,29 @@ func (ts timeSlice) hgram(b int) (*Histogram, time.Duration) {
 			bstring = fmt.Sprintf("%s - %s", low/res*res, high/res*res)
 
 			// The value didn't fit in the previous
-			// bucket, so the new bucket count should
+			// bin, so the new bin count should
 			// be incremented.
-			bucket[bstring]++
+			bin[bstring]++
 
 			pos++
 		}
 	}
 
-	*hgram = append(*hgram, bucket)
+	*hgram = append(*hgram, bin)
 
 	return hgram, interval
 }
 
 // These should be self-explanatory:
+
+func (ts timeSlice) cumulative() time.Duration {
+	var total time.Duration
+	for _, t := range ts {
+		total += t
+	}
+
+	return total
+}
 
 func (ts timeSlice) hMean() time.Duration {
 	var total float64
@@ -115,15 +125,6 @@ func (ts timeSlice) hMean() time.Duration {
 	}
 
 	return time.Duration(float64(ts.Len()) / total)
-}
-
-func (ts timeSlice) cumulative() time.Duration {
-	var total time.Duration
-	for _, t := range ts {
-		total += t
-	}
-
-	return total
 }
 
 func (ts timeSlice) avg() time.Duration {
@@ -136,6 +137,19 @@ func (ts timeSlice) avg() time.Duration {
 
 func (ts timeSlice) p(p float64) time.Duration {
 	return ts[int(float64(ts.Len())*p+0.5)-1]
+}
+
+func (ts timeSlice) stdDev() time.Duration {
+	m := ts.avg()
+	s := 0.00
+
+	for _, t := range ts {
+		s += math.Pow(float64(m-t), 2)
+	}
+
+	msq := s / float64(ts.Len())
+
+	return time.Duration(math.Sqrt(msq))
 }
 
 func (ts timeSlice) long5p() time.Duration {
